1f0e7d72138a43cc892a6f8d09af93d828e3e61a
dreamsportfootball <wangchiken0912@gmail.com>
2026-06-23 20:09:30 +0800

    refactor: centralize player profile type

---
+++ types/player.ts
@@ -0,0 +1,10 @@
+export interface PlayerProfile {
+  id: string;
+  teamId: string;
+  name: string;
+  englishName?: string;
+  number: number;
+  gender: string;
+  nationality: string;
+  age: number;
+}

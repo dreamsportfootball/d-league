1759beba50f8e69a56834025e149d8d6a136c4ae
dreamsportfootball <wangchiken0912@gmail.com>
2026-06-23 19:21:44 +0800

    refactor: add useSeason hook

---
+++ hooks/useSeason.ts
@@ -0,0 +1,12 @@
+import { useContext } from 'react';
+import { SeasonContext } from '../contexts/SeasonContext';
+
+export const useSeason = () => {
+  const context = useContext(SeasonContext);
+
+  if (!context) {
+    throw new Error('useSeason must be used within a SeasonProvider');
+  }
+
+  return context;
+};

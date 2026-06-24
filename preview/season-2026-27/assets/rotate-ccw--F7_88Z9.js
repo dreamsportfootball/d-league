6d7fa6c4bdc2419a98267e805842b7d66ae1c242
dreamsportfootball <dreamsportfootball@users.noreply.github.com>
2026-06-24 15:24:09 +0000

    preview: deploy 2026-27 season site 26d29f305311c6abfa62d124ed8045641c7db014

---
+++ preview/season-2026-27/assets/rotate-ccw--F7_88Z9.js
@@ -0,0 +1,11 @@
+import{d as a}from"./index-CObyW0Q8.js";/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M10 20a1 1 0 0 0 .553.895l2 1A1 1 0 0 0 14 21v-7a2 2 0 0 1 .517-1.341L21.74 4.67A1 1 0 0 0 21 3H3a1 1 0 0 0-.742 1.67l7.225 7.989A2 2 0 0 1 10 14z",key:"sc7q7i"}]],c=a("funnel",e);/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8",key:"1357e3"}],["path",{d:"M3 3v5h5",key:"1xhq8a"}]],n=a("rotate-ccw",o);export{c as F,n as R};

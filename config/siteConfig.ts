16b0d880525749a3fa7d1fe44a22c92880db5b42
dreamsportfootball <wangchiken0912@gmail.com>
2026-06-27 18:06:15 +0800

    config(team): limit profile pages to current season

--- config/siteConfig.ts
+++ config/siteConfig.ts
@@ -1,6 +1,7 @@
 import type { RegistrationProgressConfig, SeasonId } from '../types/season';
 
 export const CURRENT_SEASON_ID: SeasonId = '2026-27';
+export const TEAM_PROFILE_SEASON_ID: SeasonId = CURRENT_SEASON_ID;
 export const SHOW_REGISTRATION_NAV = true;
 
 export const CURRENT_REGISTRATION_PROGRESS: RegistrationProgressConfig = {

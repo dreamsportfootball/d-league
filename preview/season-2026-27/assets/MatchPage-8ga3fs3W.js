6b7c9f0624f01b1da721ce666d4bebeda1d0dd33
dreamsportfootball <dreamsportfootball@users.noreply.github.com>
2026-08-13 14:29:08 +0000

    preview: deploy 2026-27 season site 51eacc41455595ee8bcfb481340f1f173bbbc226

---
+++ preview/season-2026-27/assets/MatchPage-8ga3fs3W.js
@@ -0,0 +1,1 @@
+import{k as o,a as n,j as c,N as m}from"./index-C-lnnfGN.js";const i=()=>{const{id:a=""}=o(),[r]=n(),s=new URLSearchParams,e=r.get("season");e&&s.set("season",e),a&&s.set("match",a);const t=s.toString();return c.jsx(m,{to:`/schedule${t?`?${t}`:""}`,replace:!0})};export{i as default};

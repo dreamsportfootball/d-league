b316347a427fbe9a9ee9ac974205c01f4ca2f7cc
dreamsportfootball <wangchiken0912@gmail.com>
2026-08-23 01:13:32 +0800

    Tighten participant logo label spacing

--- components/SeasonParticipants.tsx
+++ components/SeasonParticipants.tsx
@@ -34,7 +34,7 @@ const ParticipantTeamLogo: React.FC<ParticipantTeamLogoProps> = ({ seasonId, tea
   if (!logoUrl || failed) return null;
 
   return (
-    <span className="mr-3 flex h-9 w-9 shrink-0 items-center justify-center md:mr-4 md:h-10 md:w-10" aria-hidden="true">
+    <span className="mr-2 flex h-9 w-9 shrink-0 items-center justify-center md:h-10 md:w-10" aria-hidden="true">
       <img
         src={logoUrl}
         alt=""

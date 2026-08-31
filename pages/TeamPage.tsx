6a57c359b6806fb8ee555a34100d311acb9b4e9c
dreamsportfootball <wangchiken0912@gmail.com>
2026-08-31 18:25:49 +0800

    Restore TeamPage formatting

--- pages/TeamPage.tsx
+++ pages/TeamPage.tsx
@@ -298,6 +298,7 @@ const TeamPage: React.FC = () => {
               {socialLinks.length > 0 && renderSocialLinks(true)}
             </div>
           </div>
+
           <dl className="mt-7 grid grid-cols-4 divide-x divide-neutral-300 border-t border-neutral-300 pt-4 md:mt-6">
             {[
               ['排名', seasonHasStarted && standing ? formatStatValue(standing.rank) : '-'],
